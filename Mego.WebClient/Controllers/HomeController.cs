32e157b8f5ad3659bb423e61cf4754c551079005
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-11-22 23:40:50 +0200

    Doewnload work

--- Mego.WebClient/Controllers/HomeController.cs
+++ Mego.WebClient/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using Mego.WebClient.Models.Request;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace Mego.WebClient.Controllers
 {
@@ -17,17 +19,6 @@ namespace Mego.WebClient.Controllers
             _reportService = reportService ?? throw new ArgumentNullException(nameof(reportService));
         }
 
-        /// <summary>
-        /// Download report
-        /// </summary>
-        /// <returns>List of reports in date range</returns>
-        [HttpPost("DownloadReport")]
-        //[ProducesResponseType(typeof(IList<Report>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IList<Report>>> GetAllFilteredReportsAsync([FromQuery] DateTime? from, [FromQuery] DateTime? till)
-        {
-            var reports = await _reportService.GetFilteredReportsAsync(from, till);
-            return Ok(reports);
-        }
 
         public ActionResult Index()
         {
@@ -40,8 +31,36 @@ namespace Mego.WebClient.Controllers
             if (model == null) throw new ArgumentNullException(nameof(model));
 
             var result = await _reportService.GetFilteredReportsAsync(model.DateFrom, model.DateTo);
+            var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            var fileName = $"report_{DateTime.Now}.xlsx";
+            try
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    IXLWorksheet worksheet =
+                        workbook.Worksheets.Add("Authors");
+                    worksheet.Cell(1, 1).Value = "Id";
+                    worksheet.Cell(1, 2).Value = "FirstName";
+                    worksheet.Cell(1, 3).Value = "LastName";
+                    for (int index = 1; index <= result.Count; index++)
+                    {
+                        worksheet.Cell(index + 1, 1).Value = result[index - 1].Id;
+                        worksheet.Cell(index + 1, 2).Value = result[index - 1].OrderDate;
+                        worksheet.Cell(index + 1, 3).Value = result[index - 1].SummaryPrice;
+                    }
 
-            return View();
+                    await using (var stream = new MemoryStream())
+                    {
+                        workbook.SaveAs(stream);
+                        var content = stream.ToArray();
+                        return File(content, contentType, fileName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
