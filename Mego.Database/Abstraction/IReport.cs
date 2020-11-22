b572d9bca9c00d7a48d709a39e5dec35c35c7b3a
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-11-22 15:09:43 +0200

    Template for relation servic

---
+++ Mego.Database/Abstraction/IReport.cs
@@ -0,0 +1,37 @@
+﻿using Mego.Database.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mego.Database.Abstraction
+{
+    /// <summary>
+    /// Min methods for manipulation with Report table
+    /// </summary>
+    public interface IReport
+    {
+        /// <summary>
+        /// Get all exist reports
+        /// </summary>
+        /// <returns>List of reports</returns>
+        Task<List<Report>> GetAllReportAsync();
+
+        /// <summary>
+        /// Add new report
+        /// </summary>
+        /// <param name="newReport">Report which need to add to table</param>
+        /// <returns>Report which was add in Db</returns>
+        Task<Report> CreateReportAsync(Report newReport);
+
+        /// <summary>
+        /// Update report
+        /// </summary>
+        /// <param name="report">Model with new data</param>
+        /// <returns>Updated report entity</returns>
+        Task<Report> UpdateReport(Report report);
+
+        /// <summary>
+        /// Remove record with particular Id
+        /// </summary>
+        Task RemoveReportByIdAsync();
+    }
+}
---
+++ Mego.Domain/Services/ReportService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mego.Database;
+using Mego.Database.Abstraction;
+using Mego.Database.Models;
+
+namespace Mego.Domain.Services
+{
+    public class ReportService : IReport
+    {
+        private readonly MegoDbContext _context;
+
+        public ReportService(MegoDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task<List<Report>> GetAllReportAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Report> CreateReportAsync(Report newReport)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Report> UpdateReport(Report report)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveReportByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
