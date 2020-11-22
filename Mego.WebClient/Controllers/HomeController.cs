c2dfc892f4d7aea5c043e359b34043d76d92bdc2
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-11-22 23:14:06 +0200

    Getting data from view

--- Mego.WebClient/Controllers/HomeController.cs
+++ Mego.WebClient/Controllers/HomeController.cs
@@ -1,37 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Mego.Database.Abstraction;
+using Mego.Database.Models;
+using Mego.WebClient.Models.Request;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Mego.WebClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Mego.WebClient.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IReportService _reportService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IReportService reportService)
         {
-            _logger = logger;
+            _reportService = reportService ?? throw new ArgumentNullException(nameof(reportService));
         }
 
-        public IActionResult Index()
+        /// <summary>
+        /// Download report
+        /// </summary>
+        /// <returns>List of reports in date range</returns>
+        [HttpPost("DownloadReport")]
+        //[ProducesResponseType(typeof(IList<Report>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IList<Report>>> GetAllFilteredReportsAsync([FromQuery] DateTime? from, [FromQuery] DateTime? till)
+        {
+            var reports = await _reportService.GetFilteredReportsAsync(from, till);
+            return Ok(reports);
+        }
+
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<ActionResult> Index(FilterRequest model)
         {
-            return View();
-        }
+            if (model == null) throw new ArgumentNullException(nameof(model));
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var result = await _reportService.GetFilteredReportsAsync(model.DateFrom, model.DateTo);
+
+            return View();
         }
     }
 }
---
+++ Mego.WebClient/Models/Response/ReportTableResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Mego.WebClient.Models.Response
+{
+    public class ReportTableResponse
+    {
+        public string Title { get; set; }
+        public string Price { get; set; }
+    }
+}
