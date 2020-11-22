e940c5770f3e3e1a6f78bd2d4869751d85033b53
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2020-11-22 17:43:35 +0200

    ReportController

--- Mego.Domain/Services/ReportService.cs
+++ Mego.Domain/Services/ReportService.cs
@@ -19,10 +19,8 @@ namespace Mego.Domain.Services
 
         /// <inheritdoc />
         public async Task<List<Report>> GetAllReportAsync()
-        {
-           var result = await _context.Reports.ToListAsync();
-           return result;
-        }
+            => await _context.Reports.ToListAsync();
+          
 
         /// <inheritdoc />
         public async Task<Report> CreateReportAsync(Report newReport)
--- Mego.WebClient/Controllers/ReportController.cs
+++ Mego.WebClient/Controllers/ReportController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Mego.WebClient.Controllers
 {
@@ -26,10 +27,51 @@ namespace Mego.WebClient.Controllers
         /// <returns>List of reports</returns>
         [HttpGet("GetAllReports")]
         [ProducesResponseType(typeof(IList<Report>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IList<Report>>> GetAllGroups()
+        public async Task<ActionResult<IList<Report>>> GetAllGroupsAsync()
         {
             var reports = await _reportService.GetAllReportAsync();
             return Ok(reports);
+        }
+
+        /// <summary>
+        /// Add new report
+        /// </summary>
+        /// <returns>Added report reports</returns>
+        [HttpPost("AddReport")]
+        [ProducesResponseType(typeof(Report), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Report>> AddReportAsync(Report model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            var reports = await _reportService.CreateReportAsync(model);
+            return Ok(reports);
+        }
+
+        /// <summary>
+        /// Add new report
+        /// </summary>
+        /// <returns>Added report reports</returns>
+        [HttpPost("UpdateReport")]
+        [ProducesResponseType(typeof(Report), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Report>> UpdateReportAsync(Report model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            var reports = await _reportService.UpdateReportAsync(model);
+            return Ok(reports);
+        }
+
+
+        /// <summary>
+        /// Remove report
+        /// </summary>
+        [HttpDelete("DeleteReport")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<Report>> UpdateReportAsync(int reportId)
+        {
+            await _reportService.RemoveReportByIdAsync(reportId);
+            return NoContent();
         }
     }
 }
